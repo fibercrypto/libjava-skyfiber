dc15e0ccc25cdd062ff57d2a8a444e5019395dc8
Maykel Arias Torres <dev0006@simelo.tech>
2019-01-04 06:17:40 -0500

    refs #8 Rename .travis/install-travis-gcc.sh -> .travis/install-travis.sh && modific sh

--- .travis/install-travis.sh
+++ .travis/install-travis.sh
@@ -3,6 +3,19 @@
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-get install -qq g++-6 && sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 90;
+  curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
+  chmod +x ~/bin/gimme
+
+  mkdir swig_build && \
+  cd swig_build && \
+  wget http://prdownloads.sourceforge.net/swig/swig-3.0.12.tar.gz && \
+  tar -zxf swig-3.0.12.tar.gz && \
+  cd swig-3.0.12 && \
+  sudo ./configure --prefix=/usr && \
+  sudo make && \
+  sudo make install && \
+  cd ../../ && \
+  sudo rm -rf swig_build
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
@@ -15,6 +28,10 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   git show 42d31bba7772fb01f9ba442d9ee98b33a6e7a055:Formula/gcc\@6.rb > Formula/gcc\@6.rb
   echo 'Installing gcc@6 (6.4.0-2)'
   brew install gcc\@6 || brew link --overwrite gcc\@6
+  echo 'Installing gimme'
+  brew install swig;
+  brew install gimme;
+  
 fi
 
 cd $TRAVIS_BUILD_DIR
